72e0acf48461cfce4f7b6f9deb4e6b8e89dbb2dc
Dennis Schiese <schiesedennis@googlemail.com>
2024-02-27 16:02:17 +0100

    wip

---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Controller"},{"p":"org.example","l":"JsonToModel"},{"p":"org.example","l":"JsonToModelAutomatedTest"},{"p":"org.example","l":"Main"}];updateSearchResults();
